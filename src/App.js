829348b8eb8a974ccae1f8c55544fe0782c0324b
claudio.f.raposo <claudio.f.raposo@accenture.com>
2021-08-31 20:25:27 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [users, setUsers] = useState([])
+  const api = axios.create({
+    baseURL: "https://mocknode.azurewebsites.net"
+  })
+  useEffect(()=>{
+    api.get("/users").then(
+      response => {
+        setUsers(response.data)
+      }
+    )
+  })
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React on Azure 2
-        </a>
+       {users.map(user => <p key={user.id}>{user.nome}</p>)}
       </header>
     </div>
   );
